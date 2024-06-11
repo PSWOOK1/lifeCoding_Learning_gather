e3b76717fc7b38e4098fb194acc493db24fbe6e8
PSWOOK1 <skrwlqhrdma123@naver.com>
2024-06-11 21:56:24 +0900

    python num type, 사칙연산

---
+++ python/4_1_number.py
@@ -0,0 +1,16 @@
+print(-1)
+print(0)
+print(1) # int
+print(1.1) #float
+print(1+1)
+print('1+1', 1+1)
+print('2-1', 2-1)
+print('2*2', 2*2)
+print('4/2', 4/2)
+
+import math
+print('math.sqrt(4)', math.sqrt(4)) #SQuare RooT (제곱근) / n과 n을 곱한 결과값이 4인것은? = 2.0(2를 2번 곱하면 4가 된다)
+print('math.pow(4,2)', math.pow(4,2)) #POWer (거듭제곱) / 4를 2번 곱하면? (4*4=?)
+
+import random
+print('random.random()', random.random()) # 실행 시킬 때마다 랜덤한 값 생성
